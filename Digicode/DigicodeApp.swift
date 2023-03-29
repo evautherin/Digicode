6a6c6418d3a5689ba82f00dc3455416eeb88b962
Etienne Vautherin <etienne@vautherin.com>
2023-03-29 10:36:14 +0200

    Initial Commit

---
+++ Digicode/DigicodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigicodeApp.swift
+//  Digicode
+//
+//  Created by Etienne Vautherin on 29/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DigicodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
