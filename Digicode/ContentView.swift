a283b2deac0c80b7b368e7be24a45ef65d1659e6
Etienne Vautherin <etienne@vautherin.com>
2023-03-30 10:26:23 +0200

    SignInView

--- Digicode/ContentView.swift
+++ Digicode/ContentView.swift
@@ -10,34 +10,8 @@ import FirebaseAuth
 import FirebaseAuthCombineSwift
 
 struct ContentView: View {
-    @State var email = ""
-    @State var password = ""
-
     var body: some View {
         VStack {
-//            Text(password)
-            TextField("e-mail", text: $email)
-                .keyboardType(.emailAddress)
-                .textInputAutocapitalization(.never)
-                .disableAutocorrection(true)
-
-            SecureField("Password", text: $password)
-            
-            Button("Login", action: {
-                print("e-mail: \(email), password: \(password)")
-                
-                Auth.auth().signIn(withEmail: email, password: password) {(result, error) in
-                    if let error = error {
-                        print("Error: \(error)")
-                        return
-                    }
-                    
-                    if let result = result {
-                        print("\(result.user.uid)")
-                    }
-                }
-            })
-            
             Button("Logout", action: {
                 do {
                     try Auth.auth().signOut()
@@ -47,13 +21,6 @@ struct ContentView: View {
             })
         }
         .padding()
-//        .onReceive(Auth.auth().authStateDidChangePublisher()) { user in
-//            switch user {
-//            case .none: print("Disconnected")
-//            case .some(let user): print("User \(user.uid) connected")
-//            }
-//        }
-        
     }
 }
 
---
+++ Digicode/SignInView.swift
@@ -0,0 +1,56 @@
+//
+//  SignInView.swift
+//  Digicode
+//
+//  Created by Etienne Vautherin on 30/03/2023.
+//
+
+import SwiftUI
+import FirebaseAuth
+import FirebaseAuthCombineSwift
+
+struct SignInView: View {
+    @State var email = ""
+    @State var password = ""
+
+    var body: some View {
+        VStack {
+//            Text(password)
+            TextField("e-mail", text: $email)
+                .keyboardType(.emailAddress)
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+
+            SecureField("Password", text: $password)
+            
+            Button("Login", action: {
+                print("e-mail: \(email), password: \(password)")
+                
+                Auth.auth().signIn(withEmail: email, password: password) {(result, error) in
+                    if let error = error {
+                        print("Error: \(error)")
+                        return
+                    }
+                    
+                    if let result = result {
+                        print("\(result.user.uid)")
+                    }
+                }
+            })
+        }
+        .padding()
+//        .onReceive(Auth.auth().authStateDidChangePublisher()) { user in
+//            switch user {
+//            case .none: print("Disconnected")
+//            case .some(let user): print("User \(user.uid) connected")
+//            }
+//        }
+        
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
