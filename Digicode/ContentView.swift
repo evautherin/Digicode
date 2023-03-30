b79e2adbe8a73bfb80a0ddcd6a9ad627f6f84e55
Etienne Vautherin <etienne@vautherin.com>
2023-03-30 09:39:41 +0200

    Firebase connection

--- Digicode/ContentView.swift
+++ Digicode/ContentView.swift
@@ -6,16 +6,54 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+import FirebaseAuthCombineSwift
 
 struct ContentView: View {
+    @State var email = ""
+    @State var password = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+//            Text(password)
+            TextField("e-mail", text: $email)
+                .keyboardType(.emailAddress)
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+
+            SecureField("Password", text: $password)
+            
+            Button("Login", action: {
+                print("e-mail: \(email), password: \(password)")
+                
+                Auth.auth().signIn(withEmail: email, password: password) {(result, error) in
+                    if let error = error {
+                        print("Error: \(error)")
+                        return
+                    }
+                    
+                    if let result = result {
+                        print("\(result.user.uid)")
+                    }
+                }
+            })
+            
+            Button("Logout", action: {
+                do {
+                    try Auth.auth().signOut()
+                } catch {
+                    print("Error: \(error.localizedDescription)")
+                }
+            })
         }
         .padding()
+//        .onReceive(Auth.auth().authStateDidChangePublisher()) { user in
+//            switch user {
+//            case .none: print("Disconnected")
+//            case .some(let user): print("User \(user.uid) connected")
+//            }
+//        }
+        
     }
 }
 
