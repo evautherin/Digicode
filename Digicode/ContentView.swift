b4286a5ecb4786f3dd23caa43be3501edc07a831
Etienne Vautherin <etienne@vautherin.com>
2023-03-31 15:23:29 +0200

    Map

--- Digicode/ContentView.swift
+++ Digicode/ContentView.swift
@@ -8,18 +8,24 @@
 import SwiftUI
 import FirebaseAuth
 import FirebaseAuthCombineSwift
+import MapKit
 
 struct ContentView: View {
     @StateObject var viewModel = ViewModel()
     @State var showingSheet = false
+    @State private var mapRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 48.858400, longitude: 2.294500),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
     
     var body: some View {
         NavigationStack {
             VStack {
-                List(viewModel.codes) { code in
-//                    Text(code.name)
-                    NavigationLink(code.name, value: code)
-                }
+//                List(viewModel.codes) { code in
+//                    NavigationLink(code.name, value: code)
+//                }
+                Map(coordinateRegion: $mapRegion)
+                
                 Button("Sign Out", action: ViewModel.signOut)
             }
             .navigationDestination(for: Code.self) { code in
